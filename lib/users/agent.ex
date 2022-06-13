7dbf54095bd4300f5c4255ad8f002cea95c96c3a
Daniel <daniel_sousa.p@outlook.com>
2022-06-13 00:04:22 -0300

    refactor: save file

--- lib/users/agent.ex
+++ lib/users/agent.ex
@@ -9,5 +9,14 @@ defmodule Exlivery.Users.Agent do
 
   def save(%User{} = user), do: Agent.update(__MODULE__, &update_state(&1, user))
 
+  def get(cpf), do: Agent.get(__MODULE__, &get_user(&1, cpf))
+
+  defp get_user(state, cpf) do
+    case Map.get(state, cpf) do
+      nil -> {:error, "User not found"}
+      user -> {:ok, user}
+    end
+  end
+
   defp update_state(state, %User{cpf: cpf} = user), do: Map.put(state, cpf, user)
 end
