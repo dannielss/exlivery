0582708d2fcf516e62c9d8966d1d6b5cea61426f
Daniel <daniel_sousa.p@outlook.com>
2022-06-14 23:01:24 -0300

    create agents

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,5 +1,14 @@
 defmodule Exlivery do
-  def hello do
-    :world
+  alias Exlivery.Orders.Agent, as: OrdersAgent
+  alias Exlivery.Orders.CreateOrUpdate, as: CreateOrUpdateOrders
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate, as: CreateOrUpdateUser
+
+  def start_agents do
+    UserAgent.start_link(%{})
+    OrdersAgent.start_link(%{})
   end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_order(params), to: CreateOrUpdateOrders, as: :call
 end
